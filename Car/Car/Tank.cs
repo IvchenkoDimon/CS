d104d9716cf557edd9928bc2fc121a1b762e0d6a
Student <Student@304-1>
2020-02-18 21:28:48 +0200

    CAR - in process.

---
+++ Car/Car/Tank.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Car
+{
+	class Tank
+	{
+		uint volume;//Атрибут (характеристика объекта) 
+		double fuel;//Состояние объекта
+
+		public uint Volume
+		{
+			get => volume;
+			private set => volume = value >= 40 && value <= 120 ? value : throw new Exception("Bad tank volume!");
+		}
+		public double Fuel
+		{
+			get => fuel;
+			set
+			{
+				fuel += value;
+				if (fuel < 0) fuel = 0;
+				if (fuel > volume) fuel = volume;
+			}
+		}
+
+		void Fill(double fuel)
+		{
+			Fuel += fuel;
+		}
+
+		public Tank(uint volume)
+		{
+			Volume = volume;
+			Fuel = fuel;
+		}
+		public override string ToString()
+		{
+			return $"Tank volume: {Volume};\nFuel level: {Fuel}";
+		}
+	}
+}
---
+++ Inharitance/Interfaces/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Interfaces
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+			
+        }
+    }
+}
